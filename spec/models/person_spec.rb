5c4b6d9cdce879f326f62beb0f7f767b45f79705
Lisyk <sergey.liskovoy@gmail.com>
2015-08-18 13:56:47 -0400

    fix validations test

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Person, :type => :model do
+
+  let(:person) { Person.new(first_name: 'Sergio', last_name: 'Lisiko') }
   it "is invalid without a first name" do
-    person = Person.new(first_name: nil)
+    person.first_name = nil
     expect(person).not_to be_valid
+  end
+
+  it "is invalid without a last name" do
+    person.last_name = nil 
+    expect(person).to_not be_valid
   end
 end
