cdb94f09172db5211aac07693693574f1514f3f4
Lisyk <sergey.liskovoy@gmail.com>
2015-08-18 21:38:17 -0400

    add phone number

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ group :development, :test do
 
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+
+  gem 'capybara'
 end
 
 group :production do
--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -69,6 +69,8 @@ RSpec.describe PhoneNumbersController, :type => :controller do
 
   describe "POST create" do
     describe "with valid params" do
+      let(:alice) {Person.create(first_name: 'Alice', last_name: 'Smith') }
+      let(:valid_attributes) {{number: '555-1234', person_id: alice.id}}
       it "creates a new PhoneNumber" do
         expect {
           post :create, {:phone_number => valid_attributes}, valid_session
@@ -81,9 +83,11 @@ RSpec.describe PhoneNumbersController, :type => :controller do
         expect(assigns(:phone_number)).to be_persisted
       end
 
-      it "redirects to the created phone_number" do
-        post :create, {:phone_number => valid_attributes}, valid_session
-        expect(response).to redirect_to(PhoneNumber.last)
+      it "redirects to the phone number's person" do
+        alice = Person.create(first_name: 'Lisyk', last_name: 'Lissssik')
+        valid_attributes = { number: '555-9999', person_id: alice.id}
+        post :create, { :phone_number => valid_attributes }, valid_session
+        expect(response).to redirect_to(alice)
       end
     end
 
